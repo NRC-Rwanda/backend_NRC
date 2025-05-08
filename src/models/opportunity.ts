7b723b09b4e0c2eb43c1a3faf5ca4f86c141c8f7
IRADUKUNDA SANGWA CEDRIC <110623461+Dawaic6@users.noreply.github.com>
2025-05-08 06:08:02 +0200

    check oopp

--- src/models/opportunity.ts
+++ src/models/opportunity.ts
@@ -3,15 +3,15 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface IOpportunity extends Document {
   title: string;
   shortDescription?: string; // Optional
-  image: string; // URL or file path to the image
-  link: string; // Required link for the opportunity
+  image?: string; // URL or file path to the image
+  link?: string; // Required link for the opportunity
 }
 
 const OpportunitySchema: Schema<IOpportunity> = new mongoose.Schema({
   title: { type: String, required: true },
   shortDescription: { type: String },
-  image: { type: String, required: true },
-  link: { type: String, required: true },
+  image: { type: String },
+  link: { type: String},
 });
 
 export default mongoose.model<IOpportunity>("Opportunity", OpportunitySchema);
--- src/routes/opportunityRoutes.ts
+++ src/routes/opportunityRoutes.ts
@@ -6,11 +6,47 @@ import {
   updateOpportunity,
   deleteOpportunity,
 } from "../controllers/opportunirtyController";
+import path from "path";
+import multer from "multer";
+
+// Multer configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../uploads")); // Save files to the uploads directory
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = `${Date.now()}-${file.originalname}`;
+    cb(null, uniqueSuffix); // Generate a unique filename
+  },
+});
+
+const fileFilter = (req: any, file: any, cb: any) => {
+  const allowedMimeTypes = [
+    "image/jpeg",
+    "image/png",
+    "application/pdf",
+    "video/mp4",
+    "video/quicktime", // .mov
+  ];
+
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type. Only images, PDFs, and videos are allowed."));
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+
 
 const router = express.Router();
 
 // Add a new opportunity
-router.post("/opportunities", addOpportunity);
+router.post("/opportunities",upload.fields([
+  { name: "image", maxCount: 1 }, // Allow one image file
+  { name: "video", maxCount: 1 }, // Allow one video file
+  { name: "pdf", maxCount: 1 },   // Allow one PDF file
+]), addOpportunity);
 
 // Get all opportunities
 router.get("/opportunities", getOpportunities);
@@ -19,7 +55,11 @@ router.get("/opportunities", getOpportunities);
 router.get("/opportunities/:id", getOpportunityById);
 
 // Update an opportunity
-router.put("/opportunities/:id", updateOpportunity);
+router.put("/opportunities/:id",upload.fields([
+  { name: "image", maxCount: 1 }, // Allow one image file
+  { name: "video", maxCount: 1 }, // Allow one video file
+  { name: "pdf", maxCount: 1 },   // Allow one PDF file
+]), updateOpportunity);
 
 // Delete an opportunity
 router.delete("/opportunities/:id", deleteOpportunity);
