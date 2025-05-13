02f475ff1908deb68e105cd7edb96b6eed34e830
IRADUKUNDA SANGWA CEDRIC <110623461+Dawaic6@users.noreply.github.com>
2025-05-13 20:13:07 +0200

    update events

--- src/models/announcement.ts
+++ src/models/announcement.ts
@@ -4,7 +4,9 @@ export interface IAnnouncement extends Document {
   title: string;
   shortDescription?: string; // Optional
   image?: string; // Optional URL or file path to the image
-  video?: string; // Optional URL or file path to the video
+  video?: string;
+  displayOnHomepage: boolean;
+ // Optional URL or file path to the video
   pdf?: string; // Optional URL or file path to the PDF document
   link?: string; // Optional link for the announcement
   category: "announcement" | "opportunities"; // Enum for category
@@ -15,6 +17,7 @@ const AnnouncementSchema: Schema<IAnnouncement> = new mongoose.Schema({
   shortDescription: { type: String },
   image: { type: String }, // Optional 
   video: { type: String }, // Optional
+  displayOnHomepage: { type: Boolean, default: false },
   pdf: { type: String }, // Optional
   link: { type: String }, // Optional
   category: { type: String, enum: ["announcement", "opportunities"], required: true }, // Enum
